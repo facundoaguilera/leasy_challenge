3ed6a3fb7d95ff7812ba0a08e5150cd058e0cc93
Fguilera <faguileradev@gmail.com>
2025-06-06 04:46:45 +0000

    modifico comando para crear usuarios de test

--- users/management/commands/create_test_users.py
+++ users/management/commands/create_test_users.py
@@ -5,10 +5,18 @@ from users.models import CustomUser
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         roles = ['ventas', 'operaciones', 'cobranzas']
-        
+        admin_email = "admin@leasy.test"
+        if not CustomUser.objects.filter(email= admin_email).exists():
+            admin = CustomUser.objects.create_user(
+                    email=admin_email,
+                    password='admin123',
+                    first_name="Admin"
+                )
+            self.stdout.write(self.style.SUCCESS(f'Usuario {admin_email} creado'))
         for role in roles:
             group, _ = Group.objects.get_or_create(name=role)
             email = f'{role}_user@leasy.test'
+            admin.groups.add(group)
             if not CustomUser.objects.filter(email=email).exists():
                 user = CustomUser.objects.create_user(
                     email=email,
@@ -16,6 +24,7 @@ class Command(BaseCommand):
                     first_name=role.capitalize()
                 )
                 user.groups.add(group)
+                
                 self.stdout.write(self.style.SUCCESS(f'Usuario {email} creado y asignado a {role}'))
 
         self.stdout.write(self.style.SUCCESS('Usuarios de prueba creados.'))
